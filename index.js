546aed35dc4c851eda69cb3c7b26479875565b6f
Kevin Randles <krandles@gmail.com>
2017-10-31 10:58:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments) {
   var result = []
-  for (let i = 0; i < array.length; i++) {
+  for (let i = 0; i < musicians.length; i++) {
     result.push(`${musicians[i]} plays ${instruments[i]}`)
   }
   return result
