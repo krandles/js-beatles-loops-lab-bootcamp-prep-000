f4d1c24b87eb404ba50d88c132dfd3241b73621e
Kevin Randles <krandles@gmail.com>
2017-10-31 10:57:46 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,7 @@
+function theBeatlesPlay(musicians, instruments) {
+  result = []
+  for (let i = 0; i < array.length; i++) {
+    result.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return result
+}
